7528a7b2904f822212e1d37b88754bbb917f9f82
faisalmo <lloistborn@live.com>
2017-05-27 14:50:45 +0700

    dir refaactor

--- app-server/test/lib/index.js
+++ app-server/test/lib/index.js
@@ -3,8 +3,8 @@ import assert from 'assert';
 
 import '../lib/index.js';
 
-describe('Example Node Server', () => {
-  it('should return 200', done => {
+describe('Endpoint node server', () => {
+  it('get "/" should return 200', done => {
     http.get('http://127.0.0.1:5000', res => {
       assert.equal(200, res.statusCode);
       done();
